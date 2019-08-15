ea02529c65e4f7afae2de8a8a47b14504791ebc5
Sam Serrels <doogly1@gmail.com>
2019-08-15 18:02:12 +0100

    0

--- ard_master_reader/ard_master_reader.ino
+++ ard_master_reader/ard_master_reader.ino
@@ -15,22 +15,48 @@
 void setup() {
   Wire.begin();        // join i2c bus (address optional for master)
   Serial.begin(9600);  // start serial for output
+  Serial.print("Hello");
 }
 
-const size_t bufLength = 3 * sizeof(int);
+const size_t bufLength = 5 * sizeof(int);
 
 void loop() {
-  Serial.print("---");
+
+  if (Serial.available() > 0) {
+    // read the incoming byte:
+    uint16_t val = (uint16_t)Serial.parseInt();
+    // say what you got:
+    Serial.print("I received: ");
+    Serial.println(val, DEC);
+
+    Wire.beginTransmission(8); // transmit to device
+
+    byte myArray[2];
+    myArray[0] = (val >> 8) & 0xFF;
+    myArray[1] = val & 0xFF;
+    Wire.write(myArray, 2);
+
+    Wire.write((char*)&val, sizeof(uint16_t)); // sends x
+    Wire.endTransmission(); // stop transmitting
+  }
+
   Wire.requestFrom(8, bufLength);  //from slave device #8
-  int out[3] = { -1, -1, -1};
+  int out[5] = { -1, -1, -1, -1, -1};
   char* idx = (char*)(&out[0]);
-  while (Wire.available() && idx < ((char*)(&out[0])+((char)bufLength))) { // slave may send less than requested
+  while (Wire.available() && idx < ((char*)(&out[0]) + ((char)bufLength))) { // slave may send less than requested
     char a = Wire.read();
     *idx = a; // receive a byte as character
     idx++;
   }
-  Serial.print(out[0]); Serial.print(",");
+  const float VCC = 3.28;
+  const float R1 = 4870.0;
+  const float R2 = 327.0;
+  const float Vout = (((VCC / 1023) * out[1]) * (R1 + R2)) / R2;
+  Serial.print(out[0]); Serial.print(", ");
   Serial.print(out[1]); Serial.print(",");
-  Serial.print(out[2]);Serial.println("");
-  delay(500);
+  Serial.print(out[2]); Serial.print(", ");
+  Serial.print(out[3]); Serial.print(",");
+  Serial.print(out[4]); Serial.print(", (");
+  Serial.print(Vout); Serial.println("V)");
+  delay(1000);
 }
--- attiny_PSU/attiny_PSU.ino
+++ attiny_PSU/attiny_PSU.ino
@@ -1,12 +1,22 @@
 
 #include <Wire.h>
 #define F_CPU 8000000
-int pwm = 1;
-int potinput = A2;
-int feedbackinput = A3;
-int potinputval;
-int feedbackinputval;
-int pwmval;
+uint16_t pwm = 1;
+uint16_t potinput = A2;
+uint16_t feedbackinput = A3;
+uint16_t VReq = 0;
+uint16_t Vactual = 0;
+uint16_t pwmval = 0;
+float target = 0;
+const uint16_t MAXPWM = 300;
+uint16_t pwmLimit = MAXPWM;
+
+uint16_t pwmSafetyRestTime = 0;
+
+bool SafetyReduce = 0;
+bool SafetyRise = 0;
+
+uint16_t lastVal;
 
 #define pin5 PIN_B0
 #define pin7 PIN_B2
@@ -25,77 +35,182 @@ void setup() {
   pinMode(pwm, OUTPUT);
   pinMode(potinput, INPUT);
   pinMode(feedbackinput, INPUT);
-  
- // pinMode(pin5, OUTPUT);
- // pinMode(pin7, OUTPUT);
-  
+
+  // pinMode(pin5, OUTPUT);
+  // pinMode(pin7, OUTPUT);
+
   digitalWrite(pwm, LOW);
   pwmval = 0;
 
   Wire.begin(8);                // join i2c bus with address #8
   Wire.onRequest(requestEvent); // register event
+  Wire.onReceive(receiveEvent);
 }
 
 void requestEvent() {
-  int out[3];
-  out[0] = potinputval;
-  out[1] = feedbackinputval;
-  out[2] = pwmval;
-  Wire.write((char*)out,3*sizeof(int)); // respond with message of 6 bytes
-  // as expected by master
+  uint16_t out[5];
+  out[0] = VReq;
+  out[1] = Vactual;
+  out[2] = (uint16_t)target;
+  out[3] = pwmval;
+  out[4] = pwmSafetyRestTime;
+  Wire.write((char*)out, 5 * sizeof(uint16_t));
 }
 
-void aled(){
-    if(potinputval > feedbackinputval){
-   // digitalWrite(pin7, LOW);
-   // digitalWrite(pin5, HIGH);
-  }
-  if(potinputval < feedbackinputval){
-  //  digitalWrite(pin7, HIGH);
-   // digitalWrite(pin5, LOW);
+void receiveEvent(size_t numBytes) {
+  uint16_t newT = 0;
+  byte a, b;
+  a = Wire.read();
+  b = Wire.read();
+  newT = a;
+  newT = newT << 8 | b;
+  if (newT >= 0 && newT < 1023) {
+    VReq = newT;
   }
 }
 
 void loop() {
-  potinputval = analogRead(potinput);
-  potinputval = map(potinputval, 1023, 0, 255, 0);
-  feedbackinputval = analogRead(feedbackinput);
-  feedbackinputval = map(feedbackinputval, 1023, 0, 255, 0);
+  // VReq = analogRead(potinput);
+  // VReq = map(VReq, 1023, 0, 255, 0);
+  Vactual = analogRead(feedbackinput);
+  // Vactual = map(Vactual, 1023, 0, 255, 0);
+
+  bool increasing = false;
+  bool decreasing = false;
 
 
-  aled();
-  while (potinputval > feedbackinputval) {
+  //slowly move target towards VReq.
+  if (Vactual < VReq) {
+    target += 0.001f;
+    increasing = true;
+  } else if (Vactual > VReq) {
+    target -= 0.01f;
+    decreasing = true;
+  }
+  if (target < 0 ) {
+    target = 0;
+  }
+
+  //Target can't get too far above of Vactual if increasing
+  if (increasing && Vactual < target && (target - Vactual) < 5) {
+    target = Vactual + 5;
+  }
+
+  //target can never be below Vactual if increasing
+  if (increasing && Vactual > target) {
+    target = Vactual;
+  }
+
+  //target can never be above of Vactual if decreasing
+  if (decreasing && target > Vactual) {
+    target = Vactual;
+  }
+
+  // Change pwmval based on difference between Vactual and target.
+  if ((uint16_t)target > Vactual) {
+    pwmval++;
+  } else if ((uint16_t)target < Vactual && pwmval > 0) {
+    pwmval--;
+  }
+/*
+  if (!SafetyReduce) {
+    pwmLimit = MAXPWM;
+    //We need to increase a lot. But are not
+    if (Vactual < target && target - Vactual > 10 && lastVal < target && Vactual <= lastVal) {
+      pwmSafetyRestTime++;
+    } else{
+      pwmSafetyRestTime=0;
+    }
+    if (pwmSafetyRestTime > 100) {
+      SafetyReduce = true;
+      pwmSafetyRestTime = 1000;
+    }
+  } else {
+    pwmLimit = 1;
+    if (pwmSafetyRestTime > 0) {
+      pwmSafetyRestTime--;
+    } else {
+      SafetyReduce = false;
+    }
+  }*/
+  lastVal = Vactual;
+  /*
+    //if we hit limit (target always > Vactual)
+    //progrssivly increase rest time every time this limit is hit
+    if (pwmval >= pwmLimit && pwmSafetyRestTime < (MAXPWM +1)) {
+      if (pwmSafetyRestTime == 0) {
+        pwmSafetyRestTime = 3;
+      } else {
+        pwmSafetyRestTime *= 2 ;
+      }
+    }
+
+    //reduce rest time by 1.
+    if (pwmSafetyRestTime > 0) {
+      pwmSafetyRestTime--;
+     // pwmval = 0;
+    }
+    pwmLimit = MAXPWM - pwmSafetyRestTime;
+
+    //awlays limit pwm
+  */
+  /*
+    //Hit the limit, perhaps issue or mosfet thermal shutdown, reduce to protect
+    if(pwmval >= pwmLimit && !SafetyReduce && !SafetyRise){
+      SafetyReduce = true;
+    }
+    if(SafetyReduce){
+      pwmLimit--;
+    }
+    if(SafetyReduce && pwmLimit < 5){
+      SafetyReduce = false;
+      SafetyRise = true;
+    }
+    if(SafetyRise && pwmLimit < MAXPWM){
+      pwmLimit++;
+    }else if(SafetyRise){
+      SafetyRise = false;
+    }
+  */
+
+  if (pwmval > pwmLimit) {
+    pwmval = pwmLimit;
+  }
+  analogWrite(pwm, pwmval);
+
+  /*
+    while (VReq > Vactual) {
     if (pwmval == 230) {
-      potinputval = analogRead(potinput);
-      potinputval = map(potinputval, 1023, 0, 255, 0);
-      feedbackinputval = analogRead(feedbackinput);
-      feedbackinputval = map(feedbackinputval, 1023, 0, 255, 0);
+      VReq = analogRead(potinput);
+      VReq = map(VReq, 1023, 0, 255, 0);
+      Vactual = analogRead(feedbackinput);
+      Vactual = map(Vactual, 1023, 0, 255, 0);
     }
     else {
       pwmval = pwmval + 1;
       analogWrite(pwm, pwmval);
-      potinputval = analogRead(potinput);
-      potinputval = map(potinputval, 1023, 0, 255, 0);
-      feedbackinputval = analogRead(feedbackinput);
-      feedbackinputval = map(feedbackinputval, 1023, 0, 255, 0);
+      VReq = analogRead(potinput);
+      VReq = map(VReq, 1023, 0, 255, 0);
+      Vactual = analogRead(feedbackinput);
+      Vactual = map(Vactual, 1023, 0, 255, 0);
     }
      aled();
-  }
-  while (potinputval < feedbackinputval) {
+    }
+    while (VReq < Vactual) {
     if (pwmval == 0) {
-      potinputval = analogRead(potinput);
-      potinputval = map(potinputval, 1023, 0, 255, 0);
-      feedbackinputval = analogRead(feedbackinput);
-      feedbackinputval = map(feedbackinputval, 1023, 0, 255, 0);
+      VReq = analogRead(potinput);
+      VReq = map(VReq, 1023, 0, 255, 0);
+      Vactual = analogRead(feedbackinput);
+      Vactual = map(Vactual, 1023, 0, 255, 0);
     }
     else {
       pwmval = pwmval - 1;
       analogWrite(pwm, pwmval);
-      potinputval = analogRead(potinput);
-      potinputval = map(potinputval, 1023, 0, 255, 0);
-      feedbackinputval = analogRead(feedbackinput);
-      feedbackinputval = map(feedbackinputval, 1023, 0, 255, 0);
+      VReq = analogRead(potinput);
+      VReq = map(VReq, 1023, 0, 255, 0);
+      Vactual = analogRead(feedbackinput);
+      Vactual = map(Vactual, 1023, 0, 255, 0);
     }
      aled();
-  }
+    }*/
 }
